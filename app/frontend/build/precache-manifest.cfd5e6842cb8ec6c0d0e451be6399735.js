d3323ef88b2f5426fcb8fd471d4c7058bcbc249d
Debajyoti Halder <rondebajyoti@gmail.com>
2020-08-15 21:33:07 +0530

    Add build files in app/frontend

---
+++ app/frontend/build/precache-manifest.cfd5e6842cb8ec6c0d0e451be6399735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b98a95d018d5cbf199a89f2c1dc20bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85f50788dc4899b3154",
+    "url": "/static/css/main.3e214d88.chunk.css"
+  },
+  {
+    "revision": "0e8a712a6662d03755e2",
+    "url": "/static/js/2.6a0a0dcc.chunk.js"
+  },
+  {
+    "revision": "229adf7ebad134adf1f1d64d54a21f63",
+    "url": "/static/js/2.6a0a0dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85f50788dc4899b3154",
+    "url": "/static/js/main.41815640.chunk.js"
+  },
+  {
+    "revision": "4de95324043b5ad5801d",
+    "url": "/static/js/runtime-main.039b1613.js"
+  }
+]);
